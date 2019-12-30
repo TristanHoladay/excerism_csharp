713e601793d8e4e4b977cc080f53afa088b8280a
TristanHoladay <tholaday777@gmail.com>
2019-12-30 16:25:20 -0600

    adding isogram exercise

---
+++ isogram/Isogram.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+
+        //handle case variances
+        string lowCaseWord = word.ToLower();
+
+        //check for empty string
+        if (String.IsNullOrEmpty(lowCaseWord))
+        {
+            return true;
+        }
+
+        //compare each letter of the word to every other letter
+        for(var i = 0; i < lowCaseWord.Length; i++)
+        {
+            //skip iteration if the character is a space or hyphen
+            if(lowCaseWord[i].ToString() == " " || lowCaseWord[i].ToString() == "-")
+            {
+                continue;
+            }
+
+            for(var j = 0; j < lowCaseWord.Length; j++)
+            {
+                
+                //don't compare the selected letter to itself
+                if(i == j)
+                {
+                    continue;
+                }
+
+                if(lowCaseWord[i] == lowCaseWord[j])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+}
