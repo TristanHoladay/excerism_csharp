fd62c46aa10072af58451490ffae4bd94c9e7ad5
TristanHoladay <tholaday777@gmail.com>
2020-01-07 20:46:02 -0600

    adding acronym and resistor-color exercises

---
+++ acronym/Acronym.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        string abbreviation = "";
+
+        //regex says: split only if characters are not alphanumeric and not ' OR only if character is _
+        string[] words = Regex.Split(phrase, @"[^\w+']|[_]");
+
+        //iterate through the words array
+        for(var i = 0; i < words.Length; i++)
+        {
+            //if statement put in place to handle ArgumentOutOfRange exception
+            if(words[i].Length > 0)
+            {
+                string letter = words[i].Substring(0, 1);
+                abbreviation += letter;
+            }
+        }
+
+        return abbreviation.ToUpper();
+    }
+}
---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    private static readonly string[] colors = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+    public static int ColorCode(string color)
+    {
+        //finding index of the given string by calling the Colors method which returns a string array
+        return Array.IndexOf(colors, color);
+    }
+
+    public static string[] Colors()
+    {
+        return colors;
+    }
+}
