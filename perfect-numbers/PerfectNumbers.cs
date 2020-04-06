e891785b8c746044e2d47325861189d7ecec3f87
TristanHoladay <tholaday777@gmail.com>
2020-04-06 12:05:49 -0700

    adding perfect numbers exercise

---
+++ perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        int sum;
+
+        if (number < 1)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        sum = AliquotSum(number);
+
+        if(sum == number)
+        {
+            return Classification.Perfect;
+        } 
+        else if(sum > number)
+        {
+            return Classification.Abundant;
+        }
+        else
+        {
+            return Classification.Deficient;
+        }
+
+    }
+
+    private static int AliquotSum(int number)
+    {
+        int sum = 0;
+
+        for (int factor = 1; factor < number; factor++)
+        {
+            if (number % factor == 0)
+            {
+                sum += factor;
+            }
+        }
+
+        return sum;
+    }
+}
